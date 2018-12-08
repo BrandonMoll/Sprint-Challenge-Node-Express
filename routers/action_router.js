ffc8b71d2cc720a68dce2111f5017ef1d1f2809d
BrandonMoll <brandonmoll85@gmail.com>
2018-12-08 12:14:17 -0600

    action GET and GET by ID working

--- routers/action_router.js
+++ routers/action_router.js
@@ -5,5 +5,31 @@ const router = express.Router();
 
 router.use(express.json());
 
+router.get('/', (req, res) => {
+    db.get()
+    .then(actions => {
+        res.json(actions)
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Error in GET endpoint'})
+    })
+});
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    db.get(id)
+    .then(action => {
+        if(action) {
+            res.json(action)
+        } else {
+            res.status(404).json({message: 'Action ID does not exist'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Error in GET by ID endpoint'})
+    })
+});
+
 
 module.exports = router;
