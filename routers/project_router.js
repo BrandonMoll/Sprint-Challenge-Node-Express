c77b7117ab35ef93c5bd6833224185462b9d87fe
BrandonMoll <brandonmoll85@gmail.com>
2018-12-08 12:00:25 -0600

    projects POST and DELETE working

--- routers/project_router.js
+++ routers/project_router.js
@@ -29,6 +29,42 @@ router.get('/:id', (req, res) => {
     .catch(err => {
         res.status(500).json({message: 'Error in GET by ID endpoint'})
     })
+});
+
+router.post('/', (req, res) => {
+    const project = req.body;
+
+    if(project.name.length < 128 && project.description) {
+        db.insert(project)
+        .then(idInfo => {
+            db.get(idInfo.id)
+            .then(newProject => {
+                res.json(newProject)
+            })
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Error in POST endpoint'})
+        })
+    } else {
+        res.json({message: 'if statement not passing'})
+    }
+
+});
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+
+    db.remove(id)
+    .then(count => {
+        if(count) {
+            res.json({message: 'Project Deleted'})
+        } else {
+            res.status(404).json({message: 'Project ID not found'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Error in DELETE endpoint'})
+    })
 })
 
 module.exports = router;
